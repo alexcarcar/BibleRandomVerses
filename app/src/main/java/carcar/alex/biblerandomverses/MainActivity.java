ab0a9bb9c9a81a9c5761b65253689c88b9790351
alexcarcar <alexcarcar@yahoo.com>
2015-12-03 03:03:07 -0800

    Updated launch icon

--- app/src/main/java/carcar/alex/biblerandomverses/MainActivity.java
+++ app/src/main/java/carcar/alex/biblerandomverses/MainActivity.java
@@ -1,55 +1,53 @@
 package carcar.alex.biblerandomverses;
 
-
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStreamWriter;
-import java.io.RandomAccessFile;
 
 public class MainActivity extends AppCompatActivity {
 
     private static TextView txtPassage;
-    public static final long SIZE = 2000;
-    public static final long LINES = 4;
-    public static String getPassage() {
-        String passage = "";
-        try {
-            RandomAccessFile raf = new RandomAccessFile("all.txt","r");
+    public static final long FILE_SIZE = 4289338;
+	public static final long SIZE = 2000;
+	public static final long LINES = 7;
 
-            long n = raf.length()-SIZE;
-            long pickStart = (long) Math.floor(Math.random()*n);
-            raf.seek(pickStart);
-            raf.readLine();
-            for (int i=0; i<LINES; i++) {
-                passage += raf.readLine();
-                passage += "\n";
-            }
-            raf.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return passage;
-
-    }
-
-    public void loadBibleFile() {
-        try {
-            InputStream in = this.getResources().openRawResource(R.raw.all);
-            this.getPackageResourcePath();
-            OutputStreamWriter out = new OutputStreamWriter(openFileOutput("all.txt", 0));
-            int c;
-            while ((c=in.read()) != -1) {
-                out.write(c);
-            }
-            in.close();
-            out.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+	public static String readLine(InputStream source) {
+		String line = "";
+		char c;
+		try {
+			do {
+				int i = source.read();
+				if (i==-1) break; 
+				c = (char) i;
+				if (c!='\n') line += c;
+			} while (c!='\n');
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return line;
+	}
+	
+    public String getPassage() {
+		String passage = "";
+		try {
+			InputStream source = this.getResources().openRawResource(R.raw.all);
+			long n = FILE_SIZE-SIZE;
+			long pickStart = (long) Math.floor(Math.random()*n);
+			if(source.skip(pickStart)<0) return "";
+			readLine(source);
+			for (int i=0; i<LINES; i++) {
+				passage += readLine(source);
+				passage += "\n\n";
+			}
+			source.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return passage;
     }
 
     @Override
@@ -57,13 +55,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         txtPassage=(TextView)findViewById(R.id.text_box);
-        //loadBibleFile();
         displayPassage();
     }
 
     public void displayPassage() {
-        txtPassage.setText(this.getPackageResourcePath());
-        //txtPassage.setText(getPassage());
+        txtPassage.setText(getPassage());
     }
 
     public void pickPassage(View view) {
