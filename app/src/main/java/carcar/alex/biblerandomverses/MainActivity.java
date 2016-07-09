0b3c520e977f17a9a5fb2b1170aee9ef4a5434d7
alexcarcar <alexcarcar@yahoo.com>
2016-07-09 11:25:20 -0700

    Added Initial Objects to Save Favorite Verses

--- app/src/main/java/carcar/alex/biblerandomverses/MainActivity.java
+++ app/src/main/java/carcar/alex/biblerandomverses/MainActivity.java
@@ -1,7 +1,10 @@
 package carcar.alex.biblerandomverses;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -10,64 +13,32 @@ import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final long FILE_SIZE = 4289338;
+    private static final long SIZE = 2000;
+    private static final long LINES = 5;
     private static TextView txtPassage;
-    public static final long FILE_SIZE = 4289338;
-	public static final long SIZE = 2000;
-	public static final long LINES = 5;
+    private boolean favorite = false;
+    private Long favoriteIndex = 0L;
+    private BibleFavorites bibleFavorites = null;
+    private Menu menu = null;
 
-	public static String readLine(InputStream source) {
-		String line = "";
-		char c;
-		try {
-			do {
-				int i = source.read();
-				if (i==-1) break; 
-				c = (char) i;
-				if (c!='\n') line += c;
-			} while (c!='\n');
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return line;
-	}
-	
-    public String getPassage() {
-		String passage = "";
-		try {
-			InputStream source = this.getResources().openRawResource(R.raw.all);
-			long n = FILE_SIZE-SIZE;
-			long pickStart = (long) Math.floor(Math.random()*n);
-			passage += title(pickStart) + "\n\n";
-			if(source.skip(pickStart)<0) return "";
-			readLine(source);
-			for (int i=0; i<LINES; i++) {
-				passage += readLine(source);
-				passage += "\n\n";
-			}
-			source.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return passage;
+    public static String readLine(InputStream source) {
+        String line = "";
+        char c;
+        try {
+            do {
+                int i = source.read();
+                if (i == -1) break;
+                c = (char) i;
+                if (c != '\n') line += c;
+            } while (c != '\n');
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return line;
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        txtPassage=(TextView)findViewById(R.id.text_box);
-        displayPassage();
-    }
-
-    public void displayPassage() {
-        txtPassage.setText(getPassage());
-    }
-
-    public void pickPassage(View view) {
-        displayPassage();
-    }
-	
-	private static String title(long n) {
+    private static String title(long n) {
         if (n < 3488) return "1 Corinthians 1";
         if (n < 7078) return "1 Corinthians 10";
         if (n < 10788) return "1 Corinthians 11";
@@ -1257,5 +1228,90 @@ public class MainActivity extends AppCompatActivity {
         if (n < 4283578) return "Zephaniah 1";
         if (n < 4286148) return "Zephaniah 2";
         return "Zephaniah 3";
+    }
+
+    public String getPassage() {
+        String passage = "";
+        try {
+            InputStream source = this.getResources().openRawResource(R.raw.all);
+            long n = FILE_SIZE - SIZE;
+            long pickStart = (long) Math.floor(Math.random() * n);
+            favoriteIndex = pickStart;
+
+            TextView scriptureTitle = (TextView) findViewById(R.id.pick);
+            if (scriptureTitle != null) {
+                scriptureTitle.setText(title(pickStart));
+            }
+
+            favorite = false;
+            if (menu != null) {
+                MenuItem item = menu.findItem(R.id.favClick);
+                if (item != null) {
+                    item.setIcon(R.drawable.ic_fav_off);
+                }
+            }
+
+            if (source.skip(pickStart) < 0) return "";
+            readLine(source);
+            for (int i = 0; i < LINES; i++) {
+                passage += readLine(source);
+                passage += "\n\n";
+            }
+            source.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return passage;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        this.bibleFavorites = new BibleFavorites(this);
+        setContentView(R.layout.activity_main);
+        txtPassage = (TextView) findViewById(R.id.text_box);
+        displayPassage();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public void displayPassage() {
+        txtPassage.setText(getPassage());
+    }
+
+    public void pickPassage(View view) {
+        displayPassage();
+    }
+
+    public void onAboutClick(MenuItem item) {
+        Intent intent = new Intent(this, AboutActivity.class);
+        startActivity(intent);
+    }
+
+    public void onFavClick(MenuItem item) {
+        favorite = !favorite;
+
+        if (favorite) {
+            item.setIcon(R.drawable.ic_fav_on);
+            bibleFavorites.addFavorite(favoriteIndex);
+        } else {
+            item.setIcon(R.drawable.ic_fav_off);
+            bibleFavorites.removeFavorite(favoriteIndex);
+        }
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        this.menu = menu;
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    public void pickPassage(MenuItem item) {
+        displayPassage();
     }
 }
