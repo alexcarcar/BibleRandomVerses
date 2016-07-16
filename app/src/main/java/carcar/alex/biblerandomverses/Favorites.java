cc81531fb1ee3d39c8c504e71550d3a36c3c11f7
alexcarcar <alexcarcar@yahoo.com>
2016-07-16 11:04:17 -0700

    Added Favorites List Functionality

--- app/src/main/java/carcar/alex/biblerandomverses/Favorites.java
+++ app/src/main/java/carcar/alex/biblerandomverses/Favorites.java
@@ -1,17 +1,39 @@
 package carcar.alex.biblerandomverses;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Favorites extends ListActivity {
+
+    private List<Long> favoriteIds = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_favorites);
 
-        String[] favorites = {"Fav 01", "Fav 02"};
+        BibleFavorites bibleFavorites = new BibleFavorites(this);
+        ArrayList<String> favorites = new ArrayList<>();
+        favoriteIds = bibleFavorites.getFavorites();
+        for (Long id : favoriteIds) {
+            favorites.add(BibleFavorites.title(id));
+        }
         setListAdapter(new ArrayAdapter<>(this, R.layout.activity_favorites, R.id.favorites, favorites));
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        Intent intent = new Intent(v.getContext(), MainActivity.class);
+        long phraseSelected = favoriteIds.get(position);
+        intent.putExtra("phraseSelected", phraseSelected);
+        startActivity(intent);
+        this.finish();
     }
 }
